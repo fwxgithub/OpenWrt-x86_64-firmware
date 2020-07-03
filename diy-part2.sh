3a7e61b428d03b39393f84b61be68b529b9c75d1
lish99 <67706697+lish99@users.noreply.github.com>
2020-07-04 02:31:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,5 +8,5 @@
 # Blog: https://p3terx.com
 #============================================================
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#Modify default IP
+sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
