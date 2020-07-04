c25675e0d1be1de4563d0edd7367c18fa43c8b14
lish99 <67706697+lish99@users.noreply.github.com>
2020-07-04 13:04:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,4 +9,4 @@
 #============================================================
 
 #Modify default IP
-sed -i 's/192.168.1.1/192.168.50.200/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.250/g' package/base-files/files/bin/config_generate
